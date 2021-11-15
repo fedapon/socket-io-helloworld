995048eec43d641f1dfa634d2a5cd4b221384ed1
fedapon <fedapon@gmail.com>
2021-11-14 22:52:36 -0300

    client has the posibility to choose the room to join in

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ httpServer.listen(80, function () {
 io.on('connection', (socket) => {
     console.log(`New connection ID: ${socket.id}`)
     
+    //handle room joins
+    socket.on('chat:message:join', (data) => {
+        console.log(`Connection ID: ${socket.id} - Joined to Room: ${data.room}`)
+        socket.join(data.room)
+    })
+    
     //send received message from client to all room participants
     socket.on('chat:message', (data) => {
         io.to(data.room).emit('chat:message', data)
